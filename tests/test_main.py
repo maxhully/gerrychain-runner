894418ee4b59fe9d3813761f84dccded63357746
Max Hully <max.hully@gmail.com>
2018-08-28 09:54:18 -0400

    Fixed tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,20 +11,17 @@ def queue():
     mock_run = MagicMock()
     mock_run.id = "1234"
     queue.get_next_task.return_value = mock_run
+    return queue
 
 
-@patch("runner.chain")
-def test_get_task_and_run_gets_task_from_queue(queue, mock_chain):
-    get_task_and_run(queue)
-
+def test_get_task_and_run_gets_task_from_queue(queue):
+    with patch("runner.chain"):
+        get_task_and_run(queue)
     assert queue.get_next_task.call_count == 1
 
 
-@patch("runner.chain")
-def test_runner_returns_failed_tasks_to_queue(queue, mock_chain):
-    run_chain = mock_chain.run_chain
-    run_chain.side_effect = Exception
-
-    get_task_and_run(queue)
-
+def test_runner_returns_failed_tasks_to_queue(queue):
+    with patch("runner.__main__.run_chain") as run_chain:
+        run_chain.side_effect = Exception("Mock exception")
+        get_task_and_run(queue)
     assert queue.return_failed_task.call_count == 1
