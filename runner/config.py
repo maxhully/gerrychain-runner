99d85556b25a4eee27d2659e79d3a863902b3ce3
Max Hully <max.hully@gmail.com>
2018-08-30 10:09:55 -0400

    Change config to use DNS name for redis instead of environment variables

--- runner/config.py
+++ runner/config.py
@@ -1,16 +1,12 @@
-import os
-import collections
-
-
 def get_redis_config():
     DEFAULT_CONFIG = {
-        "REDIS_SERVICE_HOST": "localhost",
+        "REDIS_SERVICE_HOST": "redis",
         "REDIS_SERVICE_PORT": 6379,
         "REDIS_DB": 0,
         "REDIS_PASSWORD": None,
         "REDIS_SSL": False,
     }
-    config = collections.ChainMap(os.environ, DEFAULT_CONFIG)
+    config = DEFAULT_CONFIG
     return {
         "host": config["REDIS_SERVICE_HOST"],
         "port": int(config["REDIS_SERVICE_PORT"]),
