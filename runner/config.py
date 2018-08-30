7a987714b6def49e68da9f5c85ef9d3a5fa8b22e
Max Hully <max.hully@gmail.com>
2018-08-29 22:19:10 -0400

    Fix Kubernetes environment variables

--- runner/config.py
+++ runner/config.py
@@ -4,16 +4,16 @@ import collections
 
 def get_redis_config():
     DEFAULT_CONFIG = {
-        "REDIS_HOST": "localhost",
-        "REDIS_PORT": 6379,
+        "REDIS_SERVICE_HOST": "localhost",
+        "REDIS_SERVICE_PORT": 6379,
         "REDIS_DB": 0,
         "REDIS_PASSWORD": None,
         "REDIS_SSL": False,
     }
     config = collections.ChainMap(os.environ, DEFAULT_CONFIG)
     return {
-        "host": config["REDIS_HOST"],
-        "port": int(config["REDIS_PORT"]),
+        "host": config["REDIS_SERVICE_HOST"],
+        "port": int(config["REDIS_SERVICE_PORT"]),
         "db": int(config["REDIS_DB"]),
         "password": config["REDIS_PASSWORD"],
         "ssl": bool(config["REDIS_SSL"]),
