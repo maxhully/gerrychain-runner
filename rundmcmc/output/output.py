7bc750370471631af9fea6ffd4805ebc480b615c
Max Hully <max.hully@gmail.com>
2018-09-06 16:26:48 -0400

    rundmcmc output changes

--- rundmcmc/output/output.py
+++ rundmcmc/output/output.py
@@ -27,11 +27,13 @@ def p_value_report(score_name, ensemble_scores, initial_plan_score):
     p_value = math.sqrt(2 * fraction_as_high)
     opposite_p_value = math.sqrt(2 * fraction_lower)
 
-    report = {'name': score_name,
-              'initial_plan_score': initial_plan_score,
-              'fraction_as_high': fraction_as_high,
-              'p_value': p_value,
-              'opposite_p_value': opposite_p_value}
+    report = {
+        "name": score_name,
+        "initial_plan_score": initial_plan_score,
+        "fraction_as_high": fraction_as_high,
+        "p_value": p_value,
+        "opposite_p_value": opposite_p_value,
+    }
     return report
 
 
@@ -39,30 +41,46 @@ class SlimPValueReport:
     scores = {
         "Efficiency Gap": efficiency_gap,
         "Mean-Median": mean_median,
-        "Mean-Thirdian": mean_thirdian
+        "Mean-Thirdian": mean_thirdian,
     }
 
-    def __init__(self, election, function=lambda epsilon: math.sqrt(2 * epsilon)):
+    def __init__(
+        self, election, function=lambda epsilon: math.sqrt(2 * epsilon), scores=None
+    ):
+        if scores is not None:
+            self.scores = scores
+
         self.election = election
-        self.counters = {name: Counter() for name in self.scores}
-        self.initial_scores = None
         self.function = function
+
+        self.counters = {name: Counter() for name in self.scores}
+        self.histograms = {name: Histogram((0, 1), 10000) for name in self.scores}
+        self.initial_scores = None
 
     def __call__(self, partition):
         election_results = partition[self.election.alias]
 
         if self.initial_scores is None:
-            self.initial_scores = {name: score(election_results)
-                                   for name, score in self.scores.items()}
+            self.initial_scores = {
+                name: score(election_results) for name, score in self.scores.items()
+            }
 
         for name, score in self.scores.items():
             value = score(election_results)
             self.counters[name].update([value >= self.initial_scores[name]])
+            self.histograms[name].record(value)
 
     def render(self):
-        return {"election": self.election.name,
-                "analysis": [self.render_score_analysis(score)
-                            for score in self.scores]}
+        output = self.render_without_histograms()
+        for record in output["analysis"]:
+            record["histogram"] = self.render_histogram(record["score"])
+        return output
+
+    def render_without_histograms(self):
+        return {
+            "election": self.election.name,
+            "analysis": [self.render_score_analysis(score) for score in self.scores],
+        }
 
     def compute_p_value(self, score_name):
         return self.function(self.fraction_as_high(score_name))
@@ -87,12 +105,19 @@ class SlimPValueReport:
     def render_score_analysis(self, score):
         first_party, *others = self.election.parties_to_columns
         party_name = format_party_name(first_party)
-        return {"score": score,
-                "interpretation": "Positive values mean an advantage for {}".format(party_name),
-                "fraction_as_high_as_original": self.fraction_as_high(score),
-                "number_as_high_as_original": self.counters[score][True],
-                "number_lower_than_original": self.counters[score][False],
-                "p": self.compute_p_value(score)}
+        return {
+            "score": score,
+            "interpretation": "Positive values mean an advantage for {}".format(
+                party_name
+            ),
+            "fraction_as_high_as_original": self.fraction_as_high(score),
+            "number_as_high_as_original": self.counters[score][True],
+            "number_lower_than_original": self.counters[score][False],
+            "p": self.compute_p_value(score),
+        }
+
+    def render_histogram(self, score):
+        return self.histograms[score].json()
 
     def __str__(self):
         return json.dumps(self.render(), indent=2)
@@ -119,33 +144,24 @@ class Histogram:
         left, right = bounds
         self.bin_size = (right - left) / number_of_bins
 
-        self.bins = self.generate_bins()
         self.counter = Counter()
 
     def record(self, value):
         """
         :value: value to record in the histogram
         """
-        return self.counter.update(self.find_bin(value))
-
-    def find_bin_index(self, value):
-        """
-        find_bin conducts a binary search to find the right bin for the given value.
-        """
-        left = self.bounds[0]
-        return math.floor((value - left) / self.bin_size)
+        return self.counter.update([self.find_bin(value)])
 
     def find_bin(self, value):
-        return self.bins[self.find_bin_index(value)]
-
-    def generate_bins(self):
         left = self.bounds[0]
-        for n in range(self.number_of_bins):
-            yield (left + n * self.bin_size, left + (n + 1) * self.bin_size)
+        bin_index = math.floor((value - left) / self.bin_size)
+        return (self.bin_size * bin_index, self.bin_size * (bin_index + 1))
 
     def csv(self):
         header = "left,right,count\n"
-        rows = ",".join((left, right, count) for (left, right), count in self.counter.items())
+        rows = ",".join(
+            (left, right, count) for (left, right), count in self.counter.items()
+        )
         body = "\n".join(rows)
         return header + body
 
@@ -197,38 +213,42 @@ def get_chain_scores(chain, handlers):
               handlers.
 
     """
-    return ({key: handler(state) for key, handler in handlers.items()}
-            for state in chain)
+    return (
+        {key: handler(state) for key, handler in handlers.items()} for state in chain
+    )
 
 
 def handle_scores_separately(chain, handlers):
     table = ChainOutputTable()
 
-    initialScores = {key: handler(chain.state) for
-            key, handler in handlers.items() if key != "flips"}
+    initialScores = {
+        key: handler(chain.state) for key, handler in handlers.items() if key != "flips"
+    }
     table.append(initialScores)
 
     nhandlers = {key: value for key, value in handlers.items() if key != "flips"}
 
-    jsonToText = '{'
+    jsonToText = "{"
     jsonSave = False
     if "flips" in list(handlers.keys()):
-        jsonToText += '"0": ' + json.dumps(handlers['flips'](chain.state))
+        jsonToText += '"0": ' + json.dumps(handlers["flips"](chain.state))
         jsonSave = True
 
     for row in get_chain_scores(chain, nhandlers):
         table.append(row)
         if jsonSave:
-            jsonToText += ", " + "\"" + str(chain.counter + 1) + "\""
-            + ": " + json.dumps(handlers["flips"](chain.state))
-    jsonToText += '}'
+            jsonToText += ", " + '"' + str(chain.counter + 1) + '"'
+            +": " + json.dumps(handlers["flips"](chain.state))
+    jsonToText += "}"
 
     return (table, jsonToText, nhandlers)
 
 
 def get_from_each(table, key):
-    return [{header: row[header][key] for header in row if key in row[header]}
-            for row in table]
+    return [
+        {header: row[header][key] for header in row if key in row[header]}
+        for row in table
+    ]
 
 
 def log_dict_as_json(hist, scores, outputFile="output.json"):
@@ -244,15 +264,14 @@ def flips_to_pngs(hist, scores, outputFile="output.png"):
 
 def log_table_to_file(table, scores, outputFile="output.txt"):
     if outputFile:
-        with open(outputFile, 'w') as f:
+        with open(outputFile, "w") as f:
             for key in scores:
                 if table[key] is not None:
                     f.write(", ".join([str(x) for x in table[key]]))
                     f.write("\n")
 
 
-def pipe_to_table(chain, handlers, display=True, number_to_display=10,
-                  bin_interval=1):
+def pipe_to_table(chain, handlers, display=True, number_to_display=10, bin_interval=1):
     table = ChainOutputTable()
 
     if number_to_display > 0:
